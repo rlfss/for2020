e1660ff7ce3753d7ba1b19897a17b6d26c75eb5e
Hammam Tara <gm@royalline.gold>
2020-02-14 00:39:51 +0200

    [ADD] app note_extended

---
+++ note_extended/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Notes Extended',
+    'version': '12.0.1.0.0',
+    'description': """Adds extra functionality to Odoo Notes""",
+    'summary': 'Auto archive and related notes',
+    'author': 'Hugo Rodrigues',
+    'website': 'https://hugorodrigues.net',
+    'license': 'Other OSI approved licence',
+    'category': 'Tools',
+    'depends': [
+        'note',
+    ],
+    'data': [
+        'views/note_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_instal': False
+}
